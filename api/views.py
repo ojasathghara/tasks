efdcdc3562f48c045eb870fce254081fd8a072ca
ojaswi825 <ojaswi.athghara98@gmail.com>
2020-06-25 15:04:35 +0530

    added CRUD functions

--- api/views.py
+++ api/views.py
@@ -1,6 +1,67 @@
 from django.shortcuts import render
-from django.http import JsonResponse
+
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from .models import Task
+from .serializers import TaskSerializer
 
 # Create your views here.
+@api_view(['GET'])
 def apiOverview(request):
-    return JsonResponse("API BASE POINT", safe=False)
+    
+    api_urls = {
+        'List':'/task-list/',
+        'Detail View':'/task-detail/<str:pk>/',
+        'Create':'/task-create/',
+        'Update':'/task-update/<str:pk>/',
+        'Delete':'/task-delete/<str:pk>/',
+    }
+
+    return Response(api_urls)
+
+@api_view(['GET'])
+def taskList(request):
+    tasks = Task.objects.all()
+    json = TaskSerializer(tasks, many=True)
+
+    return Response(json.data)
+
+@api_view(['GET'])
+def taskDetail(request, pk):
+    task = Task.objects.get(id=pk)
+    json = TaskSerializer(task, many=False)
+
+    return Response(json.data)
+
+@api_view(['POST'])
+def taskCreate(request):
+# normal request contains instance but api request contains data
+
+    json = TaskSerializer(data = request.data)
+    # just like forms
+    if json.is_valid():
+        json.save()
+
+    # automatically generates the views, put the json string in the content field, duplicate ids are handled automatically
+    return Response(json.data)
+
+@api_view(['POST'])
+def taskUpdate(request, pk):
+    task = Task.objects.get(id=pk)
+    json = TaskSerializer(instance=task, data=request.data)
+
+    if json.is_valid():
+        json.save()
+
+    return Response(json.data)  
+
+@api_view(['DELETE'])
+def taskDelete(request, pk):
+    try:
+        task = Task.objects.get(id=pk)
+        task.delete()
+    except:
+        return Response("Item does not exist!")
+
+    return Response("Item deleted")  
--- rest/settings.py
+++ rest/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+# Custom Apps    
     'api.apps.ApiConfig',
+    'rest_framework', 
 ]
 
 MIDDLEWARE = [
